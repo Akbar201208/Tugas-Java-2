40af2b2937ddb5a6b5cbb9628e806ac4e5b1234d
Akbar201208 <muhammad_firjatullah_33rpl@student.smktelkom-mlg.sch.id>
2024-08-29 15:01:55 +0700

    Add files via upload

---
+++ ManagingPeople.java
@@ -0,0 +1,21 @@
+//Driver class
+public class ManagingPeople {
+public static void main(String[] args) {
+   //name, salary
+   Employee e1 = new Employee();
+   e1.setName("Jane");
+   e1.setSalary(350000.00);
+   e1.print();
+
+   Employee e2 = new Employee();
+   e2.setName("Brandon");
+   e2.setSalary(250000.00);
+   e2.print();
+
+   Employee e3 = new Employee();
+   e3.setName("Anna");
+   e3.setSalary(250000.00);
+   e4.print();
+ } 
+}
+//buat objek class dari driver class berikut ini
